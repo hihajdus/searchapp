217ac834f85e740849d2245b29b447f41d085a13
hihajdus <sebastian.hajdus@gmail.com>
2019-05-05 16:19:19 +0200

    search component, scss install, update npm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchDetailComponent } from './search-detail/search-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchDetailComponent
   ],
   imports: [
     BrowserModule
